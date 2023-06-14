d9ab982d04a31201b2ce0f7e33a0c2b64e680b0c
budchirp <budchirp@gmail.com>
2023-06-15 01:53:29 +0300

    feat: fix mason

--- lua/oxygen/plugins/config/mason.lua
+++ lua/oxygen/plugins/config/mason.lua
@@ -1,31 +1,26 @@
 local mason = require('mason')
-local mason_lspconfig_mapping = require('mason-lspconfig.mappings.server')
 local mason_lspconfig = require('mason-lspconfig')
 
 base46.load_highlight('mason')
 
-local packages = {}
+local get_packages = function()
+  local packages = {}
 
-local installServer = function(server_name)
-  local install = config.lsp.servers[server_name].install
-  if install == nil then
-    install = true
-  end
+  for server_name, user_config in pairs(config.lsp.servers) do
+    local install = user_config.install
+    if install ~= false or install == nil then
+      install = true
+    end
 
-  return install
-end
-
-for server_name, _ in pairs(config.lsp.servers) do
-  if installServer(server_name) then
-    if mason_lspconfig_mapping[server_name] then
-      table.insert(packages, mason_lspconfig_mapping[server_name])
+    if install then
+      table.insert(packages, server_name)
     end
   end
+
+  return packages
 end
 
-mason_lspconfig.setup()
 mason.setup(utils.merge({
-  ensure_installed = packages,
   ui = {
     border = config.ui.border,
     icons = {
@@ -35,3 +30,9 @@ mason.setup(utils.merge({
     },
   },
 }, config.plugins.config['mason'] or {}))
+
+vim.print(get_packages())
+
+mason_lspconfig.setup({
+  ensure_installed = get_packages(),
+})
